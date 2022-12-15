0f4700c8434640bafe8b76dde1e9eb2d06928ce1
Hardy Griech <ntbox@gmx.net>
2022-12-15 14:39:55 +0100

    this compiles now, functioning?

--- src/cdc_debug.c
+++ src/cdc_debug.c
@@ -24,6 +24,7 @@
  */
 
 #include <stdarg.h>
+#include <stdbool.h>
 #include "pico/stdlib.h"
 #include "FreeRTOS.h"
 #include "task.h"
--- src/daplink/hic_hal/rp2040/rp2040.c
+++ src/daplink/hic_hal/rp2040/rp2040.c
@@ -48,18 +48,64 @@
 #define DP_EVENTSTAT                    0x44    // (RO)
 
 //
+// Control/Status Register Defines
+//
+//#define STICKYORUN          (1<<1)
+//#define STICKYCMP           (1<<4)
+//#define STICKYERR           (1<<5)
+#define SWDERRORS           (STICKYORUN|STICKYCMP|STICKYERR)
+
+#define DCB_DHCSR           0xE000EDF0
+
+
+// DBGSWENABLE, AHB_MASTER_DEBUG, HPROT1, no-auto-inc, need to add size...
+#define AP_MEM_CSW_SINGLE     (1 << 31) \
+                            | (1 << 29) \
+                            | (1 << 25) \
+                            | (0 << 4)
+
+#define AP_MEM_CSW_32       0b010
+
+#define AP_MEM_CSW          0x00
+#define AP_MEM_TAR          0x04
+#define AP_MEM_DRW          0x0C
+
+
+//
 // Abort Register Defines
 //
 #define DAP_ABORT           (1<<0)
-#define STKCMPCLR           (1<<1)          
-#define STKERRCLR           (1<<2)
-#define WDERRCLR            (1<<3)
-#define ORUNERRCLR          (1<<4)
+//#define STKCMPCLR           (1<<1)
+//#define STKERRCLR           (1<<2)
+//#define WDERRCLR            (1<<3)
+//#define ORUNERRCLR          (1<<4)
 #define ALLERRCLR           (STKCMPCLR|WDERRCLR|WDERRCLR|ORUNERRCLR)
 
 #define SWD_OK              0
 #define SWD_ERROR           3
 #define CHECK_OK(func)      { int rc = func; if (rc != SWD_OK) return rc; }
+
+
+struct core {
+    int                 state;
+    int                 reason;
+
+    uint32_t            dp_select_cache;
+    uint32_t            ap_mem_csw_cache;
+
+    uint32_t            breakpoints[4];
+//    struct reg          reg_cache[24];
+};
+
+
+struct core cores[2];
+
+// Core will point at whichever one is current...
+struct core *core = &cores[0];
+
+
+static void swd_targetsel(uint8_t core);
+static int core_enable_debug(void);
 
 
 
@@ -156,7 +202,7 @@ static uint8_t swd_from_dormant(void)
     SWJ_Sequence(128, selection_alert_seq);
     SWJ_Sequence(  4, zero_seq);
     SWJ_Sequence(  8, act_seq);
-    return 1;
+    return SWD_OK;
 }
 
 
@@ -165,15 +211,15 @@ static uint8_t swd_line_reset(void)
 {
     const uint8_t reset_seq[] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x03};
     SWJ_Sequence( 52, reset_seq);
-    return 1;
+    return SWD_OK;
 }
 
 
-/// taken from output of pyODC
-static uint8_t swd_read_idcode(uint32_t *id)
-{
-    return swd_read_dp(DP_IDCODE, id);
-}
+///// taken from output of pyODC
+//static uint8_t swd_read_idcode(uint32_t *id)
+//{
+//    return swd_read_dp(DP_IDCODE, id);
+//}
 
 
 static void swd_targetsel(uint8_t core)
@@ -189,7 +235,7 @@ static void swd_targetsel(uint8_t core)
     SWD_Sequence(0x80 + 5, NULL, &input);
     if (core == 0)
         SWD_Sequence(33, core_0, NULL);
-    else if (core == 0)
+    else if (core == 1)
         SWD_Sequence(33, core_1, NULL);
     else
         SWD_Sequence(33, core_rescue, NULL);
@@ -197,6 +243,196 @@ static void swd_targetsel(uint8_t core)
 }
 
 
+static int swd_read(int APnDP, int addr, uint32_t *result)
+{
+	uint8_t sts = 0;
+
+	if (APnDP == 0) {
+		sts = swd_read_ap(addr, result);
+	}
+	else {
+		sts = swd_read_dp(addr, result);
+	}
+	return (sts != 0) ? SWD_OK : SWD_ERROR;
+}   // swd_read
+
+
+static int swd_write(int APnDP, int addr, uint32_t value)
+{
+	uint8_t sts = 0;
+
+	if (APnDP == 0) {
+		sts = swd_write_ap(addr, value);
+	}
+	else {
+		sts = swd_write_dp(addr, value);
+	}
+	return (sts != 0) ? SWD_OK : SWD_ERROR;
+}   // swd_write
+
+
+static inline int dp_select_bank(int bank)
+{
+    int rc = SWD_OK;
+
+    assert(bank <= 0xf);
+
+    if ((core->dp_select_cache & 0xf) != bank) {
+        core->dp_select_cache = (core->dp_select_cache & 0xfffffff0) | bank;
+        rc = swd_write(0, DP_SELECT, core->dp_select_cache);
+    }
+    return rc;
+}   // dp_select_bank
+
+
+static int dp_read(uint32_t addr, uint32_t *res)
+{
+    int rc;
+
+    // First check to see if we are reading something where we might
+    // care about the dp_banksel
+    if ((addr & 0x0f) == 4) {
+        rc = dp_select_bank((addr & 0xf0) >> 4);
+        if (rc != SWD_OK)
+        	return rc;
+    }
+    return swd_read(0, addr & 0xf, res);
+}   // dp_read
+
+
+static int dp_write(uint32_t addr, uint32_t value)
+{
+    int rc;
+
+    // First check to see if we are writing something where we might
+    // care about the dp_banksel
+    if ((addr & 0x0f) == 4) {
+        rc = dp_select_bank((addr & 0xf0) >> 4);
+        if (rc != SWD_OK)
+        	return rc;
+    }
+    return swd_write(0, addr & 0xf, value);
+}   // dp_write
+
+
+/**
+ * @brief Select the AP and bank if we need to (note bank is bits 4-7)
+ *
+ * @param ap
+ * @param bank
+ * @return int
+ */
+static inline int ap_select_with_bank(uint ap, uint bank)
+{
+    int rc = SWD_OK;
+
+    assert((bank & 0x0f) == 0);
+    assert(bank <= 255);
+    assert(ap <= 255);
+
+    if ((ap != (core->dp_select_cache >> 24)) || (bank != (core->dp_select_cache & 0xf0))) {
+        core->dp_select_cache = (ap << 24) | bank | (core->dp_select_cache & 0xf);
+        rc = swd_write(0, DP_SELECT, core->dp_select_cache);
+    }
+    return rc;
+}   // ap_select_with_bank
+
+
+/**
+ * @brief Write a value to a given AP
+ *
+ * @param apnum
+ * @param addr
+ * @param value
+ * @return int
+ */
+int ap_write(int apnum, uint32_t addr, uint32_t value)
+{
+    int rc;
+
+    // Select the AP and bank (if needed)
+    rc = ap_select_with_bank(apnum, addr & 0xf0);
+    if (rc != SWD_OK)
+    	return rc;
+
+    // Now kick off the write (addr[3:2])...
+    rc = swd_write(1, addr & 0xc, value);
+    return rc;
+}   // ap_write
+
+
+/**
+ * @brief Update the memory csw if we need to
+ *
+ * @param value
+ */
+static inline int ap_mem_set_csw(uint32_t value) {
+//    static uint32_t ap_mem_csw_cache = 0xffffffff;
+    int rc = SWD_OK;
+
+    if (core->ap_mem_csw_cache != value) {
+        core->ap_mem_csw_cache = value;
+        rc = ap_write(0, AP_MEM_CSW, value);
+    }
+    return rc;
+}   // ap_mem_set_csw
+
+
+int mem_write32(uint32_t addr, uint32_t value)
+{
+    CHECK_OK(ap_mem_set_csw(AP_MEM_CSW_SINGLE | AP_MEM_CSW_32));
+    CHECK_OK(ap_write(0, AP_MEM_TAR, addr));
+    return ap_write(0, AP_MEM_DRW, value);
+}   // mem_write32
+
+
+/**
+ * @brief Use the rescue dp to perform a hardware reset
+ *
+ * @return int
+ */
+static int dp_rescue_reset()
+{
+    int rc;
+    uint32_t rv;
+    static const uint8_t zero[] = { 0, 0, 0, 0 };
+
+    cdc_debug_printf("Attempting rescue_dp reset\n");
+
+    swd_from_dormant();
+    swd_line_reset();
+    swd_targetsel(0xff);
+    rc = swd_read(0, DP_DPIDR, &rv);
+    if (rc != SWD_OK) {
+        cdc_debug_printf("rescue failed (DP_IDR read rc=%d)\n", rc);
+        return rc;
+    }
+
+    // Now toggle the power request which will cause the reset...
+    rc = swd_write(0, DP_CTRL_STAT, CDBGPWRUPREQ);
+    cdc_debug_printf("RESET rc=%d\n", rc);
+    rc = swd_write(0, DP_CTRL_STAT, 0);
+    cdc_debug_printf("RESET rc=%d\n", rc);
+
+    // Make sure the write completes...
+//    swd_send_bits((uint32_t *)zero, 8);
+    SWD_Sequence(8, zero, NULL);
+
+    // And delay a bit... no idea how long we need, but we need something.
+    for (int i=0; i < 2; i++) {
+//        swd_send_bits((uint32_t *)zero, 32);
+        SWD_Sequence(32, zero, NULL);
+    }
+    return SWD_OK;
+}   // dp_rescue_reset
+
+
+/**
+ * @brief Does the basic core select and then reads DP_DPIDR as required
+ *
+ * @param num
+ * @return int
+ */
 static int dp_core_select(uint8_t core)
 {
     uint32_t rv;
@@ -207,9 +443,16 @@ static int dp_core_select(uint8_t core)
     CHECK_OK(swd_read(0, DP_DPIDR, &rv));
     cdc_debug_printf("  id(%d)=%08lx\n", core, rv);
     return SWD_OK;
-}
+}   // dp_core_select
 
 
+/**
+ * @brief Select the core, but also make sure we can properly read
+ *        from it. Used in the initialisation routine.
+ *
+ * @param num
+ * @return int
+ */
 static int dp_core_select_and_confirm(uint8_t core)
 {
     uint32_t rv;
@@ -220,21 +463,84 @@ static int dp_core_select_and_confirm(uint8_t core)
     CHECK_OK(swd_read(0, DP_CTRL_STAT, &rv));
 
     return SWD_OK;
-}
+}   // dp_core_select_and_confirm
 
 
-static int core_select(uint8_t core)
+/**
+ * @brief Do everything we need to be able to utilise to the AP's
+ *
+ * This powers on the needed subdomains so that we can access the
+ * other AP's.
+ *
+ * @return int
+ */
+static int dp_power_on()
 {
-    uint32_t dlpidr;
+    uint32_t    rv;
 
-    CHECK_OK(dp_core_select(core));
-    CHECK_OK(dp_read(DP_DLPIDR, &dlpidr));
+    for (int i=0; i < 10; i++) {
+        // Attempt to power up...
+        if (dp_write(DP_CTRL_STAT, CDBGPWRUPREQ|CSYSPWRUPREQ) != SWD_OK) continue;
+        if (dp_read(DP_CTRL_STAT, &rv) != SWD_OK) continue;
+        if (rv & SWDERRORS) { dp_write(DP_ABORT, ALLERRCLR); continue; }
+        if (!(rv & CDBGPWRUPACK)) continue;
+        if (!(rv & CSYSPWRUPACK)) continue;
+        return SWD_OK;
+    }
+    return SWD_ERROR;
+}   // dp_power_on
+
+
+static const uint32_t bp_reg[4] = { 0xE0002008, 0xE000200C, 0xE0002010, 0xE0002014 };
+
+static int core_enable_debug()
+{
+    // Enable debug
+    CHECK_OK(mem_write32(DCB_DHCSR, (0xA05F << 16) | 1));
+
+    // Clear each of the breakpoints...
+    for (int i=0; i < 4; i++) {
+        CHECK_OK(mem_write32(bp_reg[i], 0));
+    }
     return SWD_OK;
-}
+}   // core_enable_debug
 
 
+static int core_select(uint8_t num)
+{
+    uint32_t dlpidr = 0;
+
+    // See if we are already selected...
+    if (core == &cores[num])
+    	return SWD_OK;
+
+    CHECK_OK(dp_core_select(num));
+
+    // Need to switch the core here for dp_read to work...
+    core = &cores[num];
+
+    // The core_select above will have set some of the SELECT bits to zero
+    //core->dp_select_cache &= 0xfffffff0;
+
+    // If that was ok we can validate the switch by checking the TINSTANCE part of
+    // DLPIDR
+    CHECK_OK(dp_read(DP_DLPIDR, &dlpidr));
+
+    // TODO: shouldn't we validate DPIDR with DLPIDR?
+    return SWD_OK;
+}   // core_select
+
+
+/**
+ * @brief Send the required sequence to reset the line and start SWD ops
+ *
+ * This routine needs to try to connect to each core and make sure it
+ * responds, it also powers up the relevant bits and sets debug enabled.
+ */
 static int dp_initialize(void)
 {
+	core = NULL;
+
     swd_from_dormant();
     int have_reset = 0;
 
@@ -253,6 +559,8 @@ static int dp_initialize(void)
                 }
             }
             // Make sure we can use dp_xxx calls...
+            core = &cores[c];
+            core->dp_select_cache = 0;
             if (dp_power_on() != SWD_OK) 
                 continue;
 
@@ -268,9 +576,11 @@ static int dp_initialize(void)
     if (dp_core_select(0) != SWD_OK) {
         return SWD_ERROR;
     }
+    core = &cores[0];
 
     return SWD_OK;
-}
+}   // dp_initialize
+
 
 /*************************************************************************************************/
 
@@ -335,3 +645,4 @@ static const target_family_descriptor_t g_rp2040_family = {
 };
 
 const target_family_descriptor_t *g_target_family = &g_rp2040_family;
+
