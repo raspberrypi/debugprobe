156a33fa016eaf6a8c9807ee260c9aa77073acec
Jonathan Bell <jonathan@raspberrypi.com>
2023-02-27 14:06:22 +0000

    freeRTOS: shrink heap by half
    A 128K reservation on a device with 256K of RAM is a bit demanding.
    Shrink to fix a link error in a DebugRel build.

--- src/FreeRTOSConfig.h
+++ src/FreeRTOSConfig.h
@@ -71,7 +71,7 @@
 /* Memory allocation related definitions. */
 #define configSUPPORT_STATIC_ALLOCATION         0
 #define configSUPPORT_DYNAMIC_ALLOCATION        1
-#define configTOTAL_HEAP_SIZE                   (128*1024)
+#define configTOTAL_HEAP_SIZE                   (64*1024)
 #define configAPPLICATION_ALLOCATED_HEAP        0
 
 /* Hook function related definitions. */
