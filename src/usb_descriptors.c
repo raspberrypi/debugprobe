c0ff91421a32df23aa3cd02cefe1850748e0e7bb
Jonathan Bell <jonathan@raspberrypi.com>
2024-03-18 16:31:55 +0000

    Debugprobe release 2.0

--- src/usb_descriptors.c
+++ src/usb_descriptors.c
@@ -48,7 +48,7 @@ tusb_desc_device_t const desc_device =
 
     .idVendor           = 0x2E8A, // Pi
     .idProduct          = 0x000c, // CMSIS-DAP Debug Probe
-    .bcdDevice          = 0x0103, // Version 01.03
+    .bcdDevice          = 0x0200, // Version 02.00
     .iManufacturer      = 0x01,
     .iProduct           = 0x02,
     .iSerialNumber      = 0x03,
