af8bcfd6bbf2182952b531dc1d34a99d52321177
Hardy Griech <ntbox@gmx.net>
2024-12-11 05:59:53 +0100

    debugging differences v1.20/21

--- include/boards/pico.h
+++ include/boards/pico.h
@@ -108,7 +108,7 @@
 #define PROBE_PIN_SWDIR          (PROBE_PIN_OFFSET + 0) // 1
 #define PROBE_PIN_SWCLK          (PROBE_PIN_OFFSET + 1) // 2
 #define PROBE_PIN_SWDIO          (PROBE_PIN_OFFSET + 2) // 3
-#define PROBE_PIN_RESET          6                      // Target reset config
+//#define PROBE_PIN_RESET          6                      // Target reset config
 // #define PROBE_MAX_KHZ         now in g_board_info.target_cfg->rt_max_swd_kHz, setup in pico::pico_prerun_board_config()
 
 // UART config (UART target -> probe)
