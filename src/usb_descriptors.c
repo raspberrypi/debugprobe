53875ec320d997a8814100d313d30a6445eabfea
Jonathan Bell <jonathan@raspberrypi.com>
2025-01-09 14:49:12 +0000

    Debugprobe release v2.2.1
    This release is a point release because unknown/stale submodule(s)
    caused a regression with long UART TX strings that subsequently could
    not be reproduced.
    
    The build artifacts for this release were compiled on a Raspberry Pi 5
    running Raspberry Pi OS (Debian 12) instead of Ubuntu 22.04 on amd64.

--- src/usb_descriptors.c
+++ src/usb_descriptors.c
@@ -48,7 +48,7 @@ tusb_desc_device_t const desc_device =
 
     .idVendor           = 0x2E8A, // Pi
     .idProduct          = 0x000c, // CMSIS-DAP Debug Probe
-    .bcdDevice          = 0x0220, // Version 02.20
+    .bcdDevice          = 0x0221, // Version 02.21
     .iManufacturer      = 0x01,
     .iProduct           = 0x02,
     .iSerialNumber      = 0x03,
