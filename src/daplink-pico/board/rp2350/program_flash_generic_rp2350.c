df5d59570c559a8ce7382364b75d503cc78bf78c
Hardy Griech <ntbox@gmx.net>
2025-02-04 17:32:58 +0100

    trying to execute bootrom function in target (no success)

--- src/daplink-pico/board/rp2350/program_flash_generic_rp2350.c
+++ src/daplink-pico/board/rp2350/program_flash_generic_rp2350.c
@@ -31,178 +31,152 @@ extern char __stop_for_target_connect_rp2350[];
 //
 // ---------------------------------------------------------------------------------------------------------------------
 
+// this are the bootrom functions
+// created with rp2350_target_find_rom_func()
 
-// These are supported by almost any SPI flash
-#define FLASHCMD_PAGE_PROGRAM     0x02
-#define FLASHCMD_READ_DATA        0x03
-#define FLASHCMD_READ_STATUS      0x05
-#define FLASHCMD_WRITE_ENABLE     0x06
-#define FLASHCMD_SECTOR_ERASE     0x20
-#define FLASHCMD_READ_SFDP        0x5a
-#define FLASHCMD_READ_JEDEC_ID    0x9f
-
-// Annoyingly, structs give much better code generation, as they re-use the base
-// pointer rather than doing a PC-relative load for each constant pointer.
-
-static ssi_hw_t *const ssi = (ssi_hw_t *) XIP_SSI_BASE;
-
-// Sanity check
-#undef static_assert
-#define static_assert(cond, x) extern int static_assert[(cond)?1:-1]
-check_hw_layout(ssi_hw_t, ssienr, SSI_SSIENR_OFFSET);
-check_hw_layout(ssi_hw_t, spi_ctrlr0, SSI_SPI_CTRLR0_OFFSET);
-
-typedef enum {
-    OUTOVER_NORMAL = 0,
-    OUTOVER_INVERT,
-    OUTOVER_LOW,
-    OUTOVER_HIGH
-} outover_t;
-
-// Flash code may be heavily interrupted (e.g. if we are running USB MSC
-// handlers concurrently with flash programming) so we control the CS pin
-// manually
-FOR_TARGET_RP2350_CODE static void flash_cs_force(outover_t over) {
-    io_rw_32 *reg = (io_rw_32 *) (IO_QSPI_BASE + IO_QSPI_GPIO_QSPI_SS_CTRL_OFFSET);
-#ifndef GENERAL_SIZE_HACKS
-    *reg = (*reg & ~IO_QSPI_GPIO_QSPI_SS_CTRL_OUTOVER_BITS)
-        | (over << IO_QSPI_GPIO_QSPI_SS_CTRL_OUTOVER_LSB);
-#else
-    // The only functions we want are FSEL (== 0 for XIP) and OUTOVER!
-    *reg = over << IO_QSPI_GPIO_QSPI_SS_CTRL_OUTOVER_LSB;
+#if 0
+0.126 (  1) - - e: 31956
+0.126 (  0) -      S R, flags 0x0005
+0.127 (  1) -        0xbc1d
+0.127 (  0) -        0x18c1
+0.127 (  0) - - e: 31964
+0.128 (  1) -      G S, flags 0x0017
+0.128 (  0) -        0xf76f
+0.129 (  1) -        0x81bf
+0.129 (  0) -        0x09c1
+0.130 (  1) -        0x7e15
+0.130 (  0) - - e: 31976
+0.130 (  0) -      S C, flags 0x0010
+0.131 (  1) -        0x7e35
+0.131 (  0) - - e: 31982
+0.132 (  1) -      L P, flags 0x0007
+0.132 (  0) -        0xf76f
+0.133 (  1) -        0x809f
+0.133 (  0) -        0x0eb9
+0.133 (  0) - - e: 31992
+0.134 (  1) -      G P, flags 0x0017
+0.134 (  0) -        0xf76f
+0.135 (  1) -        0xffef
+0.135 (  0) -        0x0acd
+0.135 (  0) -        0x7e2d
+0.136 (  1) - - e: 32004
+0.136 (  0) -      G B, flags 0x0017
+0.137 (  1) -        0xf76f
+0.137 (  0) -        0xff2f
+0.137 (  0) -        0x0b85
+0.138 (  1) -        0x7e4d
+0.138 (  0) - - e: 32016
+0.139 (  1) -      F A, flags 0x0017
+0.139 (  0) -        0xf76f
+0.139 (  0) -        0xfe6f
+0.140 (  1) -        0x0bc5
+0.140 (  0) -        0x7e25
+0.140 (  0) - - e: 32028
+0.141 (  1) -      R B, flags 0x0017
+0.142 (  1) -        0xf76f
+0.142 (  0) -        0xfdaf
+0.142 (  0) -        0x0571
+0.143 (  1) -        0x7e45
+0.143 (  0) - - e: 32040
+0.144 (  1) -      O A, flags 0x0017
+0.144 (  0) -        0xf76f
+0.144 (  0) -        0xfcef
+0.145 (  1) -        0x09b9
+0.145 (  0) -        0x7e3d
+0.145 (  0) - - e: 32052
+0.146 (  1) -      I F, flags 0x0007
+0.146 (  0) -        0xf76f
+0.147 (  1) -        0xfc2f
+0.147 (  0) -        0x0c1d
+0.147 (  0) - - e: 32062
+0.148 (  1) -      R A, flags 0x0007
+0.148 (  0) -        0xf76f
+0.149 (  1) -        0xfb8f
+0.149 (  0) -        0x0ca1
+0.149 (  0) - - e: 32072
+0.150 (  1) -      E X, flags 0x0007
+0.150 (  0) -        0xf76f
+0.151 (  1) -        0xfaef
+0.151 (  0) -        0x0d65
+0.151 (  0) - - e: 32082
+0.152 (  1) -      R E, flags 0x0007
+0.152 (  0) -        0xf76f
+0.153 (  1) -        0xfa4f
+0.153 (  0) -        0x0d0d
+0.153 (  0) - - e: 32092
+0.154 (  1) -      R P, flags 0x0007
+0.154 (  0) -        0xf76f
+0.155 (  1) -        0xf9af
+0.155 (  0) -        0x0cd1
+0.155 (  0) - - e: 32102
+0.156 (  1) -      F C, flags 0x0007
+0.156 (  0) -        0xf76f
+0.157 (  1) -        0xf90f
+0.157 (  0) -        0x3711
+0.158 (  1) - - e: 32112
+0.158 (  0) -      C X, flags 0x0007
+0.159 (  1) -        0xf76f
+0.159 (  0) -        0xf86f
+0.159 (  0) -        0x04f1
+0.160 (  1) - - e: 32122
+0.160 (  0) -      X M, flags 0x0007
+0.161 (  1) -        0xf76f
+0.161 (  0) -        0xf7cf
+0.161 (  0) -        0x04f5
+0.162 (  1) - - e: 32132
+0.162 (  0) -      F O, flags 0x0017
+0.163 (  1) -        0xf76f
+0.163 (  0) -        0xf72f
+0.163 (  0) -        0x06e3
+0.164 (  1) -        0x7e1d
+0.164 (  0) - - e: 32144
+0.165 (  1) -      R C, flags 0x0007
+0.165 (  0) -        0xf76f
+0.165 (  0) -        0xf66f
+0.166 (  1) -        0x0b61
+0.166 (  0) - - e: 32154
+0.167 (  1) -      V B, flags 0x0004
+0.167 (  0) -        0x03d5
+0.167 (  0) - - e: 32160
+0.168 (  1) -      S P, flags 0x0004
+0.168 (  0) -        0x0361
+0.169 (  1) - - e: 32166
+0.169 (  0) -      A B, flags 0x0007
+0.170 (  1) -        0xf76f
+0.170 (  0) -        0xf50f
+0.170 (  0) -        0x0e55
+0.171 (  1) - - e: 32176
+0.171 (  0) -      E B, flags 0x0007
+0.172 (  1) -        0xf76f
+0.172 (  0) -        0xf46f
+0.172 (  0) -        0x0f9d
+0.173 (  1) - - e: 32186
+0.173 (  0) -      C I, flags 0x0007
+0.174 (  1) -        0xf76f
+0.174 (  0) -        0xf3cf
+0.174 (  0) -        0x0f11
+0.175 (  1) - - e: 32196
+0.175 (  0) -      G U, flags 0x0007
+0.176 (  1) -        0xf76f
+0.176 (  0) -        0xf32f
+0.177 (  1) -        0x0ed7
+0.177 (  0) - - e: 32206
+0.177 (  0) -      S S, flags 0x0001
+0.178 (  1) -        0xb495
+0.178 (  0) - - e: 32212
+0.179 (  1) -      G R, flags 0x0040
+0.179 (  0) -        0x7df0
+0.179 (  0) - - e: 32218
+0.180 (  1) -      P T, flags 0x0040
+0.180 (  0) -        0x46bc
+0.180 (  0) - - e: 32224
+0.181 (  1) -      X F, flags 0x0040
+0.181 (  0) -        0x46c4
+0.182 (  1) - - e: 32230
+0.182 (  0) -      F D, flags 0x0040
+0.183 (  1) -        0x46c0
+0.183 (  0) - - e: 32236
 #endif
-    // Read to flush async bridge
-    (void) *reg;
-}
-
-// Put bytes from one buffer, and get bytes into another buffer.
-// These can be the same buffer.
-// If tx is NULL then send zeroes.
-// If rx is NULL then all read data will be dropped.
-//
-// If rx_skip is nonzero, this many bytes will first be consumed from the FIFO,
-// before reading a further count bytes into *rx.
-// E.g. if you have written a command+address just before calling this function.
-FOR_TARGET_RP2350_CODE static void flash_put_get(const uint8_t *tx, uint8_t *rx, size_t count, size_t rx_skip) {
-    // Make sure there is never more data in flight than the depth of the RX
-    // FIFO. Otherwise, when we are interrupted for long periods, hardware
-    // will overflow the RX FIFO.
-    const uint max_in_flight = 16 - 2; // account for data internal to SSI
-    size_t tx_count = count;
-    size_t rx_count = count;
-    while (tx_count || rx_skip || rx_count) {
-        // NB order of reads, for pessimism rather than optimism
-        uint32_t tx_level = ssi_hw->txflr;
-        uint32_t rx_level = ssi_hw->rxflr;
-        bool did_something = false; // Expect this to be folded into control flow, not register
-        if (tx_count && tx_level + rx_level < max_in_flight) {
-            ssi->dr0 = (uint32_t) (tx ? *tx++ : 0);
-            --tx_count;
-            did_something = true;
-        }
-        if (rx_level) {
-            uint8_t rxbyte = ssi->dr0;
-            did_something = true;
-            if (rx_skip) {
-                --rx_skip;
-            } else {
-                if (rx)
-                    *rx++ = rxbyte;
-                --rx_count;
-            }
-        }
-        if (!did_something)
-            break;
-    }
-    flash_cs_force(OUTOVER_HIGH);
-}
-
-// Convenience wrapper for above
-// (And it's hard for the debug host to get the tight timing between
-// cmd DR0 write and the remaining data)
-FOR_TARGET_RP2350_CODE static void flash_do_cmd(uint8_t cmd, const uint8_t *tx, uint8_t *rx, size_t count) {
-    flash_cs_force(OUTOVER_LOW);
-    ssi->dr0 = cmd;
-    flash_put_get(tx, rx, count, 1);
-}
-
-
-// Timing of this one is critical, so do not expose the symbol to debugger etc
-FOR_TARGET_RP2350_CODE static void flash_put_cmd_addr(uint8_t cmd, uint32_t addr) {
-    flash_cs_force(OUTOVER_LOW);
-    addr |= cmd << 24;
-    for (int i = 0; i < 4; ++i) {
-        ssi->dr0 = addr >> 24;
-        addr <<= 8;
-    }
-}
-
-
-// ----------------------------------------------------------------------------
-// Size determination via SFDP or JEDEC ID (best effort)
-// Relevant XKCD is 927
-
-FOR_TARGET_RP2350_CODE static void flash_read_sfdp(uint32_t addr, uint8_t *rx, size_t count) {
-    assert(addr < 0x1000000);
-    flash_put_cmd_addr(FLASHCMD_READ_SFDP, addr);
-    ssi->dr0 = 0; // dummy byte
-    flash_put_get(NULL, rx, count, 5);
-}
-
-FOR_TARGET_RP2350_CODE static uint32_t bytes_to_u32le(const uint8_t *b) {
-    return b[0] | (b[1] << 8) | (b[2] << 16) | (b[3] << 24);
-}
-
-// Return value >= 0: log 2 of flash size in bytes.
-// Return value < 0: unable to determine size.
-FOR_TARGET_RP2350_CODE static int __noinline flash_size_log2() {
-    uint8_t rxbuf[16];
-
-    // Check magic
-    flash_read_sfdp(0, rxbuf, 16);
-    if (bytes_to_u32le(rxbuf) != ('S' | ('F' << 8) | ('D' << 16) | ('P' << 24)))
-        goto sfdp_fail;
-    // Skip NPH -- we don't care about nonmandatory parameters.
-    // Offset 8 is header for mandatory parameter table
-    // | ID | MinRev | MajRev | Length in words | ptr[2] | ptr[1] | ptr[0] | unused|
-    // ID must be 0 (JEDEC) for mandatory PTH
-    if (rxbuf[8] != 0)
-        goto sfdp_fail;
-
-    uint32_t param_table_ptr = bytes_to_u32le(rxbuf + 12) & 0xffffffu;
-    flash_read_sfdp(param_table_ptr, rxbuf, 8);
-    uint32_t array_size_word = bytes_to_u32le(rxbuf + 4);
-    // MSB set: array >= 2 Gbit, encoded as log2 of number of bits
-    // MSB clear: array < 2 Gbit, encoded as direct bit count
-    if (array_size_word & (1u << 31)) {
-        array_size_word &= ~(1u << 31);
-    } else {
-        uint32_t ctr = 0;
-        array_size_word += 1;
-        while (array_size_word >>= 1)
-            ++ctr;
-        array_size_word = ctr;
-    }
-    // Sanity check... 2kbit is minimum for 2nd stage, 128 Gbit is 1000x bigger than we can XIP
-    if (array_size_word < 11 || array_size_word > 37)
-        goto sfdp_fail;
-    return array_size_word - 3;
-
-    sfdp_fail:
-    // If no SFDP, it's common to encode log2 of main array size in second
-    // byte of JEDEC ID
-    flash_do_cmd(FLASHCMD_READ_JEDEC_ID, NULL, rxbuf, 3);
-    uint8_t array_size_byte = rxbuf[2];
-    // Confusingly this is log2 of size in bytes, not bits like SFDP. Sanity check:
-    if (array_size_byte < 8 || array_size_byte > 34)
-        goto jedec_id_fail;
-    return array_size_byte;
-
-    jedec_id_fail:
-    return -1;
-}
-
-
 
 // ---------------------------------------------------------------------------------------------------------------------
 // YAPicoprobe definitions
@@ -214,28 +188,50 @@ FOR_TARGET_RP2350_CODE __attribute__((naked)) void rp2350_breakpoint(void)
 }   // rp2350_breakpoint
 
 
+FOR_TARGET_RP2350_CODE static uint32_t rp2350_test(uint32_t *buff)
+{
+    for (int i = 0;  i < 5;  ++i)
+        buff[i] = 0x12345678;
+    return 0x0815;
+}
+
+#define RT_FLAG_FUNC_ARM_SEC    0x0004
+#define RT_FLAG_FUNC_ARM_NONSEC 0x0010
+#define RT_FLAG_DATA            0x0040
+
+#define BOOTROM_TABLE_LOOKUP_OFFSET 0x18
+typedef void *(*rom_table_lookup_fn)(uint32_t code, uint32_t mask);
+#define ROM_TABLE_CODE(c1, c2) ((c1) | ((c2) << 8))
 
 FOR_TARGET_RP2350_CODE static uint32_t rp2350_flash_size(void)
 {
+    uint32_t buff[5];
+    int r = 1234;
+    rp2350_rom_get_sys_info_fn sys_info = NULL;
+
+    rom_table_lookup_fn rom_table_lookup = (rom_table_lookup_fn) (uintptr_t) *(uint16_t*) (BOOTROM_TABLE_LOOKUP_OFFSET);
+    sys_info = (rp2350_rom_get_sys_info_fn)0x09c1;
+
+    rp2350_test(buff);
+
+    // TODO if one of the below functions is called, then target seems top crash
+    // TODO must there anything additional be set!?
+
+//    sys_info = rom_table_lookup(ROM_TABLE_CODE('G', 'S'), RT_FLAG_FUNC_ARM_SEC);
+//    r = (*sys_info)(buff, 5, 0x0008);
 #if 0
-    // Fill in the rom functions...
-    rom_table_lookup_fn rom_table_lookup = (rom_table_lookup_fn)rom_hword_as_ptr(0x18);
-    uint16_t            *function_table = (uint16_t *)rom_hword_as_ptr(0x14);
-
-    rom_void_fn         _flash_exit_xip         = rom_table_lookup(function_table, fn('E', 'X'));
-    rom_void_fn         _flash_flush_cache      = rom_table_lookup(function_table, fn('F', 'C'));
-    rom_void_fn         _flash_enter_cmd_xip    = rom_table_lookup(function_table, fn('C', 'X'));
-
-    _flash_exit_xip();
-    int r = flash_size_log2();
-    _flash_flush_cache();
-    _flash_enter_cmd_xip();
-
-    return (r < 0) ? 0 : (1UL << r);
-    __BKPT(0);
-#else
-    return 8 * 1024 * 1024;
+    if (pico_processor_state_is_nonsecure()) {
+        return rom_table_lookup(ROM_TABLE_CODE('G', 'S'), RT_FLAG_FUNC_ARM_NONSEC);
+    }
+    else {
+        return rom_table_lookup(ROM_TABLE_CODE('G', 'S'), RT_FLAG_FUNC_ARM_SEC);
+    }
 #endif
+//    return rp2350_test(buff);
+//    return 0x6789;
+    return (uint32_t)rom_table_lookup;
+//    return 234567;
+    return buff[0];
 }   // rp2350_flash_size
 
 
@@ -260,6 +256,9 @@ uint32_t target_rp2350_get_external_flash_size(void)
 
     ok = target_set_state(RESET_PROGRAM);
     if (ok) {
+        printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+        rp2350_target_find_rom_func('G', 'S');
+        printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
         ok = rp2350_target_copy_flash_code();
         ok = rp2350_target_call_function(TARGET_RP2350_FLASH_SIZE, NULL, 0, TARGET_RP2350_BREAKPOINT, &res);
         target_set_state(RESET_PROGRAM);
