457e048dff0bfa96eba0581bcc637d4a031f3e4d
martamomotko <marta.momotko@raspberrypi.com>
2025-11-06 11:19:44 +0000

    FreeRTOS: configure SMP support for AutoBaud

--- src/FreeRTOSConfig.h
+++ src/FreeRTOSConfig.h
@@ -102,9 +102,11 @@
 */
 
 /* SMP port only */
-#define configNUM_CORES                         1
-#define configTICK_CORE                         1
+#define configNUMBER_OF_CORES                   2
+#define configTICK_CORE                         0
+#define configUSE_CORE_AFFINITY                 1
 #define configRUN_MULTIPLE_PRIORITIES           1
+#define configUSE_PASSIVE_IDLE_HOOK             0
 
 /* RP2040 specific */
 #define configSUPPORT_PICO_SYNC_INTEROP         1
